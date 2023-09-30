a12795b12cc04391bac2b0fd20992cfed5ee1ece
alejaa3 <141250722+alejaa3@users.noreply.github.com>
2023-09-30 10:47:48 -0500

    Update Graph.java

--- EnfermedadCobos/src/main/java/co/edu/unbosque/util/grafo/generico/Graph.java
+++ EnfermedadCobos/src/main/java/co/edu/unbosque/util/grafo/generico/Graph.java
@@ -20,6 +20,22 @@ public class Graph {
 	public void setListOfNodes(MyLinkedList<Vertex<?>> listOfNodes) {
 		this.listOfNodes = listOfNodes;
 	}
+
+	public static void addBidirectionalEdge(int u, int v){
+        
+		listOfNodes.get(v).getInfo().addEdge(new Edge(listOfNodes.get(v).getInfo(),
+				listOfNodes.get(u).getInfo(), 1));
+		listOfNodes.get(u).getInfo().addEdge(new Edge(listOfNodes.get(u).getInfo(),
+				listOfNodes.get(v).getInfo(), 1));
+
+    }
+    public static void addUnidirectionalEdge(int origin, int destination){
+    	
+    	listOfNodes.get(origin).getInfo().addEdge(new Edge(listOfNodes.get(origin).getInfo(),
+    			listOfNodes.get(destination).getInfo(), 1));
+
+    	
+    }
 	
 	@Override
 	public String toString() {
